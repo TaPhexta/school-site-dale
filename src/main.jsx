38e7192f3078e2993049ffcd95c65a751af8e04e
Oyama <oysmbatani@gmail.com>
2026-07-21 09:18:07 +0200

    removed google fonts and added self hosted fonts (.woff2) in order to use inter and cinzel. Next task: populate json data images and content

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,12 @@ import router from "./router";
 
 import "./styles/global.css";
 
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/700.css";
+
+import "@fontsource/cinzel/400.css";
+import "@fontsource/cinzel/700.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
